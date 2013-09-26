a5a3c0507f0afe876928e29eab8ea479b7df25ff
nja652 <nja6522@live.com>
2013-09-26 16:53:05 -0600

    Update buildandcheck.py

--- buildandcheck.py
+++ buildandcheck.py
@@ -4,16 +4,19 @@ from autojenkins import Jenkins
  
 j = Jenkins('http://192.168.1.216:8080/')
 
-#Build new job.
+# Build new job.
 print "Building Job"
 j.build('my-new-job')
- 
-#check result...Currently does not work.
-while j.last_result('my-new-job')['result'] != '':
-	result = j.last_result('my-new-job')['result']
-	#print result
-	if result == 'SUCCESS':
-		print 'Job Built Successfully'
-		break   
+
+while  j.job_info('my-new-job')['color'] == 'notbuilt' or j.job_info('my-new-job')['color'] == 'notbuilt_anime':
+	#print j.job_info('my-new-job')['color']
+	time.sleep(1)
+	if j.job_info('my-new-job')['color'] == 'blue' or j.job_info('my-new-job')['color'] == 'yellow' or j.job_info('my-new-job')['color'] == 'red':
+		#print j.job_info('my-new-job')['color']
+		print 'Build Completed'
+		break
+
+if j.last_result('my-new-job')['result'] == 'SUCCESS':
+	print 'Job was built successfully'
 else:
-	time.sleep(1)	
+	print 'Build failed'	
